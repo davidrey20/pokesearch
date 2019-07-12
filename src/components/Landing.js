3ae8b70e3b98750d312cf017cdc5643168ac904a
davidrey20 <davidrey20@gmail.com>
2019-07-12 22:04:14 +0800

    Fixed for Android

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,14 +1,31 @@
 import React from "react";
 import { View, Platform, ImageBackground, Text } from "react-native";
-import { Button, Header, Left, Icon } from "native-base";
+import { Button } from "native-base";
+import { Ionicons } from "@expo/vector-icons";
+import {
+  HeaderButtons,
+  HeaderButton,
+  Item
+} from "react-navigation-header-buttons";
+
+const IoniconsHeaderButton = props => (
+  // the `passMeFurther` variable here contains props from <Item .../> as well as <HeaderButtons ... />
+  // and it is important to pass those props to `HeaderButton`
+  // then you may add some information like icon size or color (if you use icons)
+  <HeaderButton {...props} IconComponent={Ionicons} iconSize={23} />
+);
 
 class Landing extends React.Component {
   static navigationOptions = {
     title: "Home",
     headerLeft: (
-      <Button transparent>
-        <Icon name="menu" />
-      </Button>
+      <HeaderButtons
+        left
+        HeaderButtonComponent={IoniconsHeaderButton}
+        color="black"
+      >
+        <Item title="menu" iconName="ios-menu" onPress={() => alert("menu")} />
+      </HeaderButtons>
     )
   };
   render() {
@@ -19,16 +36,14 @@ class Landing extends React.Component {
           resizeMode="contain"
           source={require("../static/landing.jpg")}
         >
-          <View style={styles.backgroundTitle}>
-            <Text style={styles.titleStyle}>Welcome to PokeSearch</Text>
-          </View>
+          <Text style={styles.titleStyle}>Welcome to PokeSearch</Text>
           <Button
             block
             info
             style={styles.buttonStyle}
             onPress={() => this.props.navigation.navigate("Search")}
           >
-            <Text>Start Searching</Text>
+            <Text style={styles.buttonTextStyle}>Start Searching</Text>
           </Button>
         </ImageBackground>
       </View>
@@ -41,25 +56,30 @@ export default Landing;
 const styles = {
   container: {
     flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
     marginTop: Platform.OS === "android" ? 24 : 0,
     marginLeft: 10,
-    marginRight: 10
+    marginRight: 10,
+    marginBottom: Platform.OS == "android" ? 10 : 0
   },
   imageBackground: {
     width: "100%",
-    height: "100%"
-  },
-  backgroundTitle: {
-    marginTop: 300,
+    height: "100%",
     justifyContent: "center",
     alignItems: "center"
   },
   titleStyle: {
     fontSize: 30,
-    color: "#3a5ca1",
-    alignItems: "center"
+    color: "#3a5ca1"
   },
   buttonStyle: {
     backgroundColor: "#3a5ca1"
+  },
+  buttonTextStyle: {
+    color: "white"
+  },
+  iconStyle: {
+    marginTop: 10
   }
 };
--- src/navigation/AppContainer.js
+++ src/navigation/AppContainer.js
@@ -2,10 +2,13 @@ import { createAppContainer, createStackNavigator } from "react-navigation";
 import Landing from "../components/Landing";
 import Search from "../components/Search";
 
-const AppNavigator = createStackNavigator({
-  Landing: { screen: Landing },
-  Search: { screen: Search }
-});
+const AppNavigator = createStackNavigator(
+  {
+    Landing: { screen: Landing },
+    Search: { screen: Search }
+  },
+  { headerLayoutPreset: "center" }
+);
 
 const AppContainer = createAppContainer(AppNavigator);
 
